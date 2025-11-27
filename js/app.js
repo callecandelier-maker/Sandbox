e70160c488f1887013ff894a533b0b7716353fec
Candelier82 <98266407+Candelier82@users.noreply.github.com>
2025-11-27 21:17:53 +0100

    click function on fields

--- js/app.js
+++ js/app.js
@@ -1,19 +1,7 @@
+const wrappers = document.querySelectorAll('.image-wrapper');
 
-
-   const cars = ['Volvo', 'BMW', 'Audi']
-
-   // För varje bil i arrayen cars – skriv ut bilens namn
-   for (let car of cars) {
-     console.log(car);
-   }
-
-
-   for (let row = 0; row < 10; row++) {
-     let line = "";
-
-     for (let col = 1; col <= 10; col++) {
-       line += col + " ";
-     }
-     console.log(line);
-   }
-
+wrappers.forEach(wrapper => {
+  wrapper.addEventListener('click', () => {
+    wrapper.classList.toggle('active'); // lägger till/tar bort 'active'
+  });
+});
